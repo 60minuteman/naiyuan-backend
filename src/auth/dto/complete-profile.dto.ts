198f99d4030fec0b93fe96d056a916092d6dab60
MelfiTech <talktomelfi@gmail.com>
2024-11-09 10:18:16 +0100

    update

--- src/auth/dto/complete-profile.dto.ts
+++ src/auth/dto/complete-profile.dto.ts
@@ -11,8 +11,8 @@ export class CompleteProfileDto {
 
   @IsOptional()
   @IsString()
-  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
-    message: 'Date must be in YYYY-MM-DD format'
+  @Matches(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,3})?([+-]\d{2}:?\d{2}|Z)$/, {
+    message: 'Date must be in ISO-8601 DateTime format (e.g. 1996-09-08T00:00:00Z)'
   })
   dateOfBirth?: string;
-} 
+}
